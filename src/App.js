7afa08741d1640d31c68c2249e4925c0b7cc1361
Risal Walangadi <risal.walangadi@gmail.com>
2020-09-20 04:40:46 +0800

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Docker</p>
+        <p>Hello from Docker</p>
       </header>
     </div>
   );
